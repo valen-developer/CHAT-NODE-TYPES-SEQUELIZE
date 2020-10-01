3a06e64d146c70de39190402a7cc977a1df041f3
valen <valenreta.developer@gmail.com>
2020-10-01 04:31:15 +0200

    dbpass

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const DBCONFIG = {
   database: process.env.DBNAME || "chat",
   user: process.env.DBUSER || "valen",
-  password: process.env.DBPASSWORD || "dbpass",
+  password: process.env.DBPASSWORD || "pass",
   host: process.env.DBHOST || "localhost",  
 };
